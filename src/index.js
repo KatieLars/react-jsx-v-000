792bdf837124ed752d7690afc5c128b905baf71f
github username <github email address>
2018-02-17 21:01:39 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {message} from './components/foo';
+import fruits from './components/fruits';
+import Tweet from './components/Tweet';
+
+ReactDOM.render(
+  <Tweet />,
+  document.getElementById('root')
+);
